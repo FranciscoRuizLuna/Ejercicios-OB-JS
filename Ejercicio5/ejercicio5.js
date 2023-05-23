730cd83a7c2129fb4a59cffc7f10b63abab3b2f9
Francisco Ruiz <franciscoruizluna@hotmail.com>
2023-05-23 16:44:41 -0600

    Realizando el ejercicio 5 del curso

---
+++ Ejercicio5/ejercicio5.js
@@ -0,0 +1,17 @@
+let alturaCentimetros = 170;
+console.log(alturaCentimetros + " centimetros");
+
+let alturaMetros = 1.70;
+console.log(alturaMetros+ " metros") ;
+
+let pesoKilogramos = 63.3;
+console.log(pesoKilogramos + " kilogramos");
+
+let alturaMetrosRedondeada = Math.round(1.70);
+console.log(alturaMetrosRedondeada + " metros redondeado");
+
+let pesoKilogramosRedondeada = Math.floor(63.3);
+console.log(pesoKilogramosRedondeada + " kilogramos redondeado");
+
+let maximoValor = Number.MAX_VALUE
+console.log((maximoValor+1)==maximoValor);
